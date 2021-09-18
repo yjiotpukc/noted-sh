f969786ac1ce6f79e9217475ca8a563807e906bb
Vadim Zubko <yjiotpukc@gmail.com>
2021-09-18 13:32:03 +0300

    Added table processing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::io::{self, BufRead, BufReader, Result, Write};
+
+const FIRST_COLUMN_COLOR: &str = "\x1B[38;5;163m";
+const RESET_COLOR: &str = "\x1B[39;00m";
 
 fn main() -> Result<()> {
     process_input()
@@ -22,5 +27,21 @@ fn process_input() -> Result<()> {
 }
 
 fn process_line(line: String) -> String {
-    line
+    process_table(line)
+}
+
+fn process_table(line: String) -> String {
+    lazy_static! {
+        static ref MATCH_RE: Regex =
+            Regex::new(r"^\s*.*?\s*(\|\s*.*?\s*)+$").expect("Regular expression is invalid");
+        static ref REPLACE_RE: Regex =
+            Regex::new(r"^(?P<fcpre>\s*)(?P<fc>.*?)(?P<fcpost>\s*)\|(?P<rem>.*?)$").unwrap();
+    }
+
+    if !MATCH_RE.is_match(&line) {
+        return line;
+    }
+
+    let replace_string = format!("$fcpre{}$fc{}$fcpost│$rem", FIRST_COLUMN_COLOR, RESET_COLOR);
+    REPLACE_RE.replace_all(&line, replace_string).to_string()
 }
