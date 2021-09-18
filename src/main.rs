9055b8eaea951505cef8ba1a37ffa9b845b1f456
Vadim Zubko <yjiotpukc@gmail.com>
2021-09-18 14:02:24 +0300

    Added heading processing

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use std::io::{self, BufRead, BufReader, Result, Write};
 
-const FIRST_COLUMN_COLOR: &str = "\x1B[38;5;163m";
+const FIRST_COLUMN_COLOR: &str = "\x1B[38;5;214m";
 const RESET_COLOR: &str = "\x1B[39;00m";
+
+const MAX_HEADING_LEVEL: usize = 1;
+const HEADING_1_COLOR: &str = "\x1B[48;5;25;97;01m";
 
 fn main() -> Result<()> {
     process_input()
@@ -27,7 +30,7 @@ fn process_input() -> Result<()> {
 }
 
 fn process_line(line: String) -> String {
-    process_table(line)
+    process_heading(process_table(line))
 }
 
 fn process_table(line: String) -> String {
@@ -43,5 +46,31 @@ fn process_table(line: String) -> String {
     }
 
     let replace_string = format!("$fcpre{}$fc{}$fcpost│$rem", FIRST_COLUMN_COLOR, RESET_COLOR);
+    REPLACE_RE.replace_all(&line, replace_string).to_string()
+}
+
+fn process_heading(line: String) -> String {
+    lazy_static! {
+        static ref MATCH_RE: Regex =
+            Regex::new(r"^(#+) .+$").expect("Regular expression is invalid");
+        static ref REPLACE_RE: Regex = Regex::new(r"^#+ (?P<htext>.*)$").unwrap();
+    }
+
+    if !MATCH_RE.is_match(&line) {
+        return line;
+    }
+
+    let captures = MATCH_RE.captures(&line).unwrap();
+    let mut heading_level = captures.get(1).unwrap().as_str().len();
+    if heading_level > MAX_HEADING_LEVEL {
+        heading_level = MAX_HEADING_LEVEL;
+    }
+
+    let heading_color = match heading_level {
+        1 => HEADING_1_COLOR,
+        _ => "",
+    };
+
+    let replace_string = format!("{}$htext{}", heading_color, RESET_COLOR);
     REPLACE_RE.replace_all(&line, replace_string).to_string()
 }
